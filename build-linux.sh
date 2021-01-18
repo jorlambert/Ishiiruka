3ad60757289367780b557f01c6dd5e9874713028
Bird <39280243+Birdthulu@users.noreply.github.com>
2021-01-18 01:50:29 -0500

    Github Actions

---
+++ build-linux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+# build-linux.sh
+
+CMAKE_FLAGS='-DLINUX_LOCAL_DEV=true'
+
+DATA_SYS_PATH="./Data/Sys/"
+BINARY_PATH="./build/Binaries/"
+
+# Move into the build directory, run CMake, and compile the project
+mkdir -p build
+pushd build
+cmake ${CMAKE_FLAGS} ../
+make -j$(nproc)
+popd
+
+# Copy the Sys folder in
+cp -r -n ${DATA_SYS_PATH} ${BINARY_PATH}
+
+touch ./build/Binaries/portable.txt
